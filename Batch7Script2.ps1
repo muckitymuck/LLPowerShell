f72dbb4e36cd7ec9a6936b5062205a68c03529d3
muckity <andrewgoldstein@hotmail.com>
2020-11-22 23:19:14 -0600

    update code 2, x

--- Batch7Script2.ps1
+++ Batch7Script2.ps1
@@ -3,11 +3,12 @@ Do {
         cls
         write-host ""
         Write-Host "1 - List the details of local PC BIOS"
-        Write-Host "2 - "
+        Write-Host "2 - List Disk Partition Info in a separate Window"
         Write-Host "3 - "
         Write-Host "4 - "
         Write-Host "5 - "
         Write-Host "6 - "
+        Write-Host "X to Exit"
         Write-Host "Type your choice and press Enter:  " -NoNewline
         
         $choice=Read-Host
@@ -30,6 +31,13 @@ Do {
             Write-Host "You chose 1"
             Write-Host "Getting BIOS info:   "
             Get-WmiObject -Class Win32_BIOS
+            pause
+        }
+        "2"
+        {
+            Write-Host "You chose 2"
+            Write-Host "Getting Disk Partition info"
+            get-Partition | Out-GridView
             pause
         }
 
